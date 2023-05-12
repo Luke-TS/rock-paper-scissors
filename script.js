db397530d5509a3691db82850eb2bcb596a220a7
Luke-TS <132500533+Luke-TS@users.noreply.github.com>
2023-05-12 10:51:37 -0500

    project complete
    created evaluatResult and playRound

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+function playRound() {
+    let comp = getComputerChoice();
+    let user = getUserInput();
+    console.log(evaluateResult(user, comp));
+    return;
+}
+
 function getComputerChoice() {
     const options = ['rock', 'paper', 'scissors'];
     return options[Math.floor(Math.random() * options.length)];
@@ -6,4 +13,18 @@ function getComputerChoice() {
 function getUserInput() {
     let userAns = prompt('rock, paper, scissors: ');
     return userAns.toLowerCase();
+}
+
+function evaluateResult(user, comp) {
+    let result;
+    if (user == 'rock' && comp == 'scissors' || user == 'scissors' && comp == 'paper' || user == 'paper' && comp == 'rock') {
+        result = `You win! ${user} beats ${comp}`;
+    }
+    else if (user == comp) {
+        result = `Tie! You both selected ${user}`;
+    }
+    else {
+        result = `You lose! ${comp} beats ${user}`;
+    }
+    return result;
 }
