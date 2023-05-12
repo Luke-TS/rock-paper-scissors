1c22d56cd500b7606c71246df63bb39e8b71642e
Luke-TS <132500533+Luke-TS@users.noreply.github.com>
2023-05-12 10:23:35 -0500

    getUserInput function complete

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function getComputerChoice() {
     const options = ['rock', 'paper', 'scissors'];
     return options[Math.floor(Math.random() * options.length)];
 }
+
+function getUserInput() {
+    let userAns = prompt('rock, paper, scissors: ');
+    return userAns.toLowerCase();
+}
